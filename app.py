d30532cffd6836e89377ef01c901727aa8e30ecf
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-05 23:52:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the trained Random Forest model
-with open('random_forest_model_better_accuracy.pkl', 'rb') as f:
+with open('https://github.com/harshalpanchal2000/Drug_Classifier-/blob/main/random_forest_model_better_accuracy.pkl', 'rb') as f:
     model = pickle.load(f)
 
 def main():
