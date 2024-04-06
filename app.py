0ff1c37506ff26b1bddfd307627ca9c7b77fe6be
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-05 23:54:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import streamlit as st
-import pandas as pd
 import pickle
 import requests
 from io import BytesIO
 
-# Function to download the file from the URL
-def download_file(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return BytesIO(response.content)
-    else:
-        st.error("Failed to download the file.")
+# URL to the pickle file
+pickle_url = "https://github.com/harshalpanchal2000/Drug_Classifier-/blob/main/random_forest_model_better_accuracy.pkl"
 
-# URL of the file in the GitHub repository
-url = 'https://github.com/harshalpanchal2000/Drug_Classifier-/blob/main/random_forest_model_better_accuracy.pkl'
-
-# Download the file
-file_content = download_file(url)
+# Download the pickle file from the URL
+response = requests.get(pickle_url)
+file_content = BytesIO(response.content)
 
 # Load the trained Random Forest model
-if file_content:
-    model = pickle.load(file_content)
+model = pickle.load(file_content)
 
 def main():
     st.title("Hello, World!")
